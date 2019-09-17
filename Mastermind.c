4a19eedea6c8733bc4126669722fc12c2fe84bfc
ThrynPerso <55408885+ThrynPerso@users.noreply.github.com>
2019-09-17 09:03:10 +0200

    Add files via upload
    Added global variables

--- Mastermind.c
+++ Mastermind.c
@@ -1,94 +1,119 @@
 #include <stdio.h>
-float solution[4]
-float playerAnswer[4]
-int try
+#include <string.h>
+#include "global.h"
 
 	int main(){
+		int reponseToInt;
 		char reponse[30] = {0};
-		printf("Bienvenue dans le jeu de Mastermind !");
-   		printf("-------------------------------------");
-		printf("Les règles sont simples :");
-		printf("Essayez de devinez la combinaison de couleurs");
-		printf("Le programme vous diras quand vous misez juste (ou pas)");
-		printf("-------------------------------------");
-		printf("Vous devez répondre sous la forme 'couleur, couleur, couleur, couleur'");
-		printf("Soit 4 couleurs parmis orange, rouge, jaune, bleu et vert");
-		printf("Par exemple 'jaune, rouge, vert, vert'");
-		printf("-------------------------------------");
-		printf("A tout moment, tapez 'SAUVEGARDER' pour enregistrer la partie afin de la reprendre plus tard");
-		printf("-------------------------------------");
-		printf("Prêt ?");
-		printf("1. Commencer une nouvelle partie");
-		printf("2. Charger une partie existant");
-		scanf("%f", reponse)
-		getMenuReponse(reponse)
+		printf("Bienvenue dans le jeu de Mastermind !\n");
+   		printf("-------------------------------------\n");
+		printf("Les regles sont simples :\n");
+		printf("Essayez de devinez la combinaison de couleurs\n");
+		printf("Le programme vous diras quand vous misez juste (ou pas)\n");
+		printf("-------------------------------------\n");
+		printf("Vous devez repondre sous la forme 'couleur, couleur, couleur, couleur'\n");
+		printf("Soit 4 couleurs parmis orange, rouge, jaune, bleu et vert\n");
+		printf("Par exemple 'jaune, rouge, vert, vert'\n");
+		printf("-------------------------------------\n");
+		printf("A tout moment, tapez 'SAUVEGARDER' pour enregistrer la partie afin de la reprendre plus tard\n");
+		printf("-------------------------------------\n");
+		printf("Pret ?\n");
+		printf("1. Commencer une nouvelle partie\n");
+		printf("2. Charger une partie existant\n");
+		scanf("%f", reponse);
+		reponseToInt = reponse - '0';
+		getMenuReponse(reponseToInt);
 
    return 0;
 	}
 
-	int getMenuReponse(char answer){
-		char location[200]
-		switch(answer) (
-			case '1' :
+	void getMenuReponse(int answer){
+		char location[200];
+		switch(answer) {
+			case 1 :
 			startNewGame();
 			break;
-			case '2' :
-			printf("Veuillez entrer la localisation du fichier");
-			scanf("%c", location);
-			loadPartie(location);
+			case 2 :
+			loadPartie();
 			break;
 			default :
 			printf("Mauvaise entrée !");
 			main();
-			)
+			};
 	return 0;
 	}
 
-	int loadPartie(char loc){
+	void loadPartie(){
 
 	}
 
-	int startNewGame(char loc){
-		int i
-		char rep
-		solution[0] = rand() % 5 + 1
-		solution[1] = rand() % 5 + 1
-		solution[2] = rand() % 5 + 1
-		solution[3] = rand() % 5 + 1
+	void startNewGame(){
+		int i;
+		char rep[40];
+		solution[0] = rand() % 5 + 1;
+		solution[1] = rand() % 5 + 1;
+		solution[2] = rand() % 5 + 1;
+		solution[3] = rand() % 5 + 1;
 		for (i = 0 ; i < 4 ; i++)
 		{
-		switch(
-			case solution[i] = 1
-			solution[i] = "jaune"
+		switch (solution[i]){
+			case 1 :
+			solution[i] = "jaune";
 			break;
-			case solution[i] = 2
-			solution[i] = "rouge"
+			case 2 :
+			solution[i] = "rouge";
 			break;
-			case solution[i] = 3
-			solution[i] = "vert"
+			case 3 :
+			solution[i] = "vert";
 			break;
-			case solution[i] = 4
-			solution[i] = "bleu"
+			case 4 :
+			solution[i] = "bleu";
 			break;
-			case solution[i] = 5
-			solution[i] = "orange"
+			case 5 :
+			solution[i] = "orange";
 			break;
-			)
+			};
 		}
+	
+		printf("-------------------------------------");
+		printf("Veuillez rentrer vos couleurs ! Premier essai");
+		printf("-------------------------------------");
+		scanf("%a", rep);
+		checkAnswer(rep);
 
-		printf("-------------------------------------")
-		printf("Veuillez rentrez vos couleurs ! Premier essai")
-		printf("-------------------------------------")
-		scanf("%a", rep)
-		checkAnswer(rep)
-
-	return 0;
+	;
 	}
 
-	char checkAnswer(char rep){
+	void checkAnswer(char * rep){
 		
 	}
 
-	void manche(float previousAnswer){
+	void saveFile(char rep){
+		char saveData[150];
+		FILE * savePath = fopen("/saves/sauvegarde.txt", "w");
+		strcpy(saveData, x);
+		strcat(saveData, ",");
+		strcat(saveData, y);
+		strcat(saveData, ",");
+		strcat(saveData, numTries);
+		strcat(saveData, ",");
+		strcat(saveData, solution[0]);
+		strcat(saveData, ",");
+		strcat(saveData, solution[1]);
+		strcat(saveData, ",");
+		strcat(saveData, solution[2]);
+		strcat(saveData, ",");
+		strcat(saveData, solution[3]);
 
+		fputs(saveData, savePath);
+		fclose(savePath);
+		printf("--------------------");
+		printf("Fichier sauvegardé !");
+		printf("--------------------");
+	}
+
+	void manche(float previousAnswer){
+		char theAnswer[40];
+		scanf("votre réponse (couleur,couleur,couleur,couleur) : %a", theAnswer);
+		checkAnswer(theAnswer);
 	}
---
+++ global.h
@@ -0,0 +1,5 @@
+char solution[4]; //Solution à trouver
+char playerAnswer[4]; //
+int numTries;
+int x; //Correct mal placés
+int y; //Correct bien placés
